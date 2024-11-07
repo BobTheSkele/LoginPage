2f9ff0e1227736baa33dcfe41e46d8cc579e7a0c
BobTheSkele <72051080+BobTheSkele@users.noreply.github.com>
2024-11-07 10:49:41 -0500

    script.js

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ function login() {
     const password = document.getElementById("password").value;
     const errorMessage = document.getElementById("error-message");
 
-    // Set the desired username and password here
+    // Set the correct username and password
     const correctUsername = "Drew";
     const correctPassword = "Howdyshell";
 
     // Check if the entered credentials are correct
     if (username === correctUsername && password === correctPassword) {
-        // Redirect to the Google Docs link
-        window.location.href = "https://docs.google.com/document/d/1Tj_b0_asUkBTjDaA62jFJdLrlBCc2_thev9MP8AHmsg/edit?usp=sharing";
+        // Redirect to the Google Drive PDF link
+        window.location.href = "https://drive.google.com/file/d/1O8rLUZsWmYvU-mP6FMBc9p72a0dBWwqJ/view?usp=sharing";
     } else {
         // Show an error message if credentials are incorrect
         errorMessage.textContent = "Incorrect username or password. Try again.";
